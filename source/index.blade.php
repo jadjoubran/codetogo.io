a3c7f2ec64c25e1f3d78fbd6ecbcf727a119279a
Jad Joubran <joubran.jad@gmail.com>
2017-12-04 20:54:06 +0100

    Design fixes

--- source/index.blade.php
+++ source/index.blade.php
@@ -10,19 +10,25 @@
     <h4>Popular use cases</h4>
 
     <div class="card">
-        <h3>How to loop through array in JavaScript</h3>
+        <a href="/how-to-loop-through-array-in-javascript/">
+            <h3>How to loop through array in JavaScript</h3>
+        </a>
         <h5>Last updated Nov 13, 2017</h5>
 
         @include('_howto.loop-through-array')
     </div>
     <div class="card">
-        <h3>How to add class to element in JavaScript</h3>
+        <a href="/how-to-add-class-to-element-in-javascript/">
+            <h3>How to add class to element in JavaScript</h3>
+        </a>
         <h5>Last updated Nov 13, 2017</h5>
 
         @include('_howto.add-class-to-element')
     </div>
     <div class="card">
-        <h3>How to find element by id in JavaScript</h3>
+        <a href="/how-to-find-element-by-id-in-javascript/">
+            <h3>How to find element by id in JavaScript</h3>
+        </a>
         <h5>Last updated Nov 13, 2017</h5>
 
         @include('_howto.find-element-by-id')
