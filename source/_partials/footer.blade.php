eed4bd73c024cf5df2185cdd4befd605c8bc3a9b
Jad Joubran <joubran.jad@gmail.com>
2018-03-08 20:16:11 +0100

    Update footer with contributors link. Closes #105

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -2,10 +2,10 @@
     <div class="container">
         <div class="footer-container">
             <div>
-                &copy; {{(new DateTime())->format('Y')}} / Built by
-                <a href="https://jadjoubran.io" target="_blank" rel="noopener">Jad Joubran</a>
-                &amp; designed by
-                <a href="https://nicolesaidy.com" target="_blank" rel="noopener">Nicole Saidy</a>
+                Designed by
+                <a href="https://nicolesaidy.com" target="_blank" rel="noopener">Nicole Saidy</a>, 
+                built by
+                <a href="https://jadjoubran.io" target="_blank" rel="noopener">Jad Joubran</a> &amp; <a href="https://github.com/jadjoubran/codetogo.io/graphs/contributors" target="_blank" rel="nofollower">contributors</a>
             </div>
             <div class="links">
                 <a href="/about">
