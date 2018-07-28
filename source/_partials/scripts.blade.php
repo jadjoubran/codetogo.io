7e8bf051f493e1f22239a7c0df0152e4b898b33f
Jad Joubran <joubran.jad@gmail.com>
2018-07-28 12:39:04 +0200

    Add cookie consent

--- source/_partials/scripts.blade.php
+++ source/_partials/scripts.blade.php
@@ -9,10 +9,62 @@
 <script defer src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.min.js"></script>
 <script defer src="{{ elixir('js/all.js', 'build') }}"></script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-110654751-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'UA-110654751-1');
+<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
+<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
+<script>
+window.addEventListener("load", function(){
+    function initAnalytics(){
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-110654751-1');
+    }
+    
+    window.cookieconsent.initialise({
+    "palette": {
+    "popup": {
+      "background": "#ffffff",
+      "text": "#8999a3"
+    },
+    "button": {
+      "background": "#cccccc",
+      "text": "#ffffff"
+    }
+  },
+  "theme": "classic",
+  "type": "opt-in",
+  onInitialise: function (status) {
+  var type = this.options.type;
+  var didConsent = this.hasConsented();
+  if (type == 'opt-in' && didConsent) {
+    initAnalytics();
+  }
+  if (type == 'opt-out' && !didConsent) {
+    // cookies already disabled
+  }
+},
+ 
+onStatusChange: function(status, chosenBefore) {
+  var type = this.options.type;
+  var didConsent = this.hasConsented();
+  if (type == 'opt-in' && didConsent) {
+    initAnalytics();
+  }
+  if (type == 'opt-out' && !didConsent) {
+    // cookies already disabled
+  }
+},
+ 
+onRevokeChoice: function() {
+  var type = this.options.type;
+  if (type == 'opt-in') {
+    // cookies already disabled
+  }
+  if (type == 'opt-out') {
+    initAnalytics();
+  }
+},
+})});
 </script>
