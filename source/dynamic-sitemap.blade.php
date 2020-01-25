b609a54ba299483fa87b2ddcd4648b042e4a26ff
jadjoubran <joubran.jad@gmail.com>
2020-01-25 16:05:08 +0100

    Update sitemap

--- source/dynamic-sitemap.blade.php
+++ source/dynamic-sitemap.blade.php
@@ -1,9 +1,11 @@
 https://codetogo.io/
 https://codetogo.io/all/
 https://codetogo.io/about/
+https://codetogo.io/javascript/
 @foreach ($jsusecases as $usecase)
 https://codetogo.io/{{ $usecase->url() }}/
 @endforeach
+https://codetogo.io/react/
 @foreach ($reactusecases as $usecase)
 https://codetogo.io/{{ $usecase->url() }}/
 @endforeach
