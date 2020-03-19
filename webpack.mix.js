28cdce14f0beb6febf776c30ca4333cd2b974b1c
jadjoubran <joubran.jad@gmail.com>
2020-03-19 12:57:39 +0100

    Remove revisionning for now - breaks cloudflare cache

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,25 @@
-let mix = require('laravel-mix');
-let build = require('./tasks/build.js');
+let mix = require("laravel-mix");
+let build = require("./tasks/build.js");
 
 mix.disableSuccessNotifications();
-mix.setPublicPath('source/assets/build');
+mix.setPublicPath("source/assets/build");
 mix.webpackConfig({
     plugins: [
         build.jigsaw,
         build.browserSync(),
-        build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss', '!source/**/_tmp/*']),
+        build.watch([
+            "source/**/*.md",
+            "source/**/*.php",
+            "source/**/*.scss",
+            "!source/**/_tmp/*"
+        ])
     ]
 });
 
-mix.js('source/_assets/js/main.js', 'js')
-    .sass('source/_assets/sass/main.scss', 'css')
+mix.js("source/_assets/js/main.js", "js")
+    .sass("source/_assets/sass/main.scss", "css")
     .options({
-        processCssUrls: false,
-    }).version();
+        processCssUrls: false
+    });
+// turned off .version(); because it would invalidate the entire cloudflare cache
+// also because the JS is rarely updated
