0de18de16bac991c31e0d41bea43dde6a7b4b495
Jad <joubran.jad@gmail.com>
2024-12-24 12:39:41 +0100

    Update nicole's website

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="footer-container">
             <div>
                 Designed by
-                <a href="https://nicolesaidy.com" target="_blank" rel="noopener">Nicole Saidy</a>, 
+                <a href="https://nicolesaidy.design target="_blank" rel="noopener">Nicole Saidy</a>, 
                 built by
                 <a href="https://jadjoubran.io" target="_blank" rel="noopener">Jad Joubran</a> &amp; <a href="https://github.com/jadjoubran/codetogo.io/graphs/contributors" target="_blank" rel="noopener nofollower">contributors</a>
             </div>
